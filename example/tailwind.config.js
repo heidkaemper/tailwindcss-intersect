56eb33b37003189a87852e11870d215eff260b56
Lars Heidkämper <lars.heidkaemper@gmail.com>
2022-02-24 17:38:09 +0100

    fix tailwind config

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    // ...
+    content: [
+        './example/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
     plugins: [
         require('../src/plugin.js')
     ],
