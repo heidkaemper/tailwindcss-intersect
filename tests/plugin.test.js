f7f26f9e08dd56815a7940a54b4a6e4a72b61cf8
Lars Heidkämper <heidkaemper@new-data-services.de>
2023-01-19 20:32:17 +0100

    add tests

---
+++ tests/plugin.test.js
@@ -0,0 +1,46 @@
+/* eslint-env jest */
+
+const path = require('path');
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+
+function run(config, plugin = tailwindcss) {
+    const { currentTestName } = expect.getState();
+
+    config = {
+        plugins: [require('./../src/plugin.js')],
+        corePlugins: { preflight: false },
+        ...config,
+    };
+
+    return postcss(plugin(config)).process('@tailwind utilities', {
+        from: `${path.resolve(__filename)}?test=${currentTestName}`,
+    });
+}
+
+it('should add variants', () => {
+    return run({
+        content: [{ raw: String.raw`<div class="intersect:opacity-50 intersect:hover:opacity-100"></div>` }],
+    }).then(result => {
+        expect(result.css).toMatchCss(String.raw`
+            .intersect\:opacity-50:not([no-intersect]) {
+                opacity: 0.5;
+            }
+            .intersect\:hover\:opacity-100:hover:not([no-intersect]) {
+                opacity: 1;
+            }
+        `);
+    });
+});
+
+it('should add arbitrary values', () => {
+    return run({
+        content: [{ raw: String.raw`<div class="intersect:left-[100px]"></div>` }],
+    }).then(result => {
+        expect(result.css).toMatchCss(String.raw`
+            .intersect\:left-\[100px\]:not([no-intersect]) {
+                left: 100px;
+            }
+        `);
+    });
+});
